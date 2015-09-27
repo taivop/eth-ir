0a81a0e9f4c1d504e9135ffd44f41f0df494d150
taivop <taivo.pungas@gmail.com>
2015-09-27 12:12:18 +0200

    fix #2 resolve problem with ngrm not seen in model; add some testing and reporting

--- src/ex2/testModels.scala
+++ src/ex2/testModels.scala
@@ -26,7 +26,18 @@ object testModels {
 
     // Calculate log-likelihood
     var llh = 0.0
-    result.toMap.foreach(pair => pair match { case (ngram, occurrences) => llh = llh + occurrences * model(ngram)})
+    var num_missing_llh_values = 0
+    result.toMap.foreach(pair => pair match { case (ngram, occurrences) => {
+      val ngram_llh_result = model.get(ngram)
+      ngram_llh_result match {
+        case Some(x) => llh += occurrences * x
+        case None => num_missing_llh_values += occurrences
+      }
+    }})
+
+    // Take into account missing values
+    val num_total_ngrams = result.size
+    llh = llh * num_total_ngrams / (num_total_ngrams - num_missing_llh_values)
 
     llh
   }
@@ -54,7 +65,9 @@ object testModels {
 
     // Run through all test cases
     var tries = MutMap[String, Int]()
-    var successes = MutMap[String, Int]()
+    var true_positives = MutMap[String, Int]()
+    var false_positives = MutMap[String, Int]()
+    var failures = Array[String]()
 
     Source.fromFile(filename).getLines().foreach(line => {
       val split = line.replace("\\s$", "").split(";", 2)
@@ -63,16 +76,33 @@ object testModels {
 
       tries(lang) = tries.getOrElse(lang, 0) + 1
 
-      if(lang == detectLanguage(models, sentence))
-        successes(lang) = successes.getOrElse(lang, 0) + 1
+      val detectedLang = detectLanguage(models, sentence)
+      if(lang == detectedLang)
+        true_positives(lang) = true_positives.getOrElse(lang, 0) + 1
+      else {
+        false_positives(detectedLang) = false_positives.getOrElse(detectedLang, 0) + 1
+        failures = failures :+ "guessed %s, correct %s | %s".format(detectedLang, lang, sentence)
+      }
     })
 
     // Print summary
-    println("Test summary:")
+    print("Test summary:\n\n")
+
+    val total_tp = true_positives.toMap.map(pair => pair match {case (lang, n) => n}).sum
+    val total_fp = false_positives.toMap.map(pair => pair match {case (lang, n) => n}).sum
+    val total_tries = tries.toMap.map(pair => pair match {case (lang, n) => n}).sum
+
+    print("ALL\t%2d/%2d TP, %2d FP, %5.1f%% accuracy\n\n".format(
+      total_tp, total_tries, total_fp, 100*total_tp/total_tries.toFloat))
+
     tries.keys.foreach(lang => {
-      val accuracy = 100 * successes.getOrElse(lang, 0) / tries(lang).toFloat
-      print("%s\t%d/%d correct, %.1f%% accuracy\n".format(lang, successes.getOrElse(lang, 0), tries(lang), accuracy))
+      val accuracy = 100 * true_positives.getOrElse(lang, 0) / tries(lang).toFloat
+      print("%s\t%2d/%2d TP, %2d FP, %5.1f%% accuracy\n".format(
+        lang, true_positives.getOrElse(lang, 0), tries(lang), false_positives.getOrElse(lang, 0), accuracy))
     })
+
+    print("\nFailures:\n")
+    failures.foreach(println)
   }
 
   def main(args: Array[String]): Unit = {
